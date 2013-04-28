33f470c5007e0ec9dad8942773cd6cc56569c185
Jim Xue <develorcer@gmail.com>
2013-04-28 20:59:17 +0800

    install_dep(): download/install dependencies in a single step

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ from Cython.Distutils import build_ext
 from distutils.core import setup
 from distutils.extension import Extension
 import os
+from os.path import basename
+from urlparse import urlsplit
+import urllib2
+import platform
+import zipfile
 
 # Third-party packages
 # Nothing for now...
@@ -48,6 +53,69 @@ def get_modules(path=None):
                 if name and name not in excluded_modules:
                     yield name, f
 
+def url2name(url):
+    return basename(urlsplit(url)[2])
+
+def download(url, local_file_name = None):
+    local_name = url2name(url)
+    req = urllib2.Request(url)
+    r = urllib2.urlopen(req)
+    if r.info().has_key('Content-Disposition'):
+        # If the response has Content-Disposition, we take file name from it
+        local_name = r.info()['Content-Disposition'].split('filename=')[1]
+        if local_name[0] == '"' or local_name[0] == "'":
+            local_name = local_name[1:-1]
+    elif r.url != url:
+        # if we were redirected, the real file name we take from the final URL
+        local_name = url2name(r.url)
+    if local_file_name:
+        # we can force to save the file as specified name
+        local_name = local_file_name
+    f = open(local_name, 'wb')
+    f.write(r.read())
+    f.close()
+
+DEP_LIST_URL = ''
+DEP_LIST_LOCAL = 'dep_list'
+PYGLET_LOCAL = 'pyglet.zip'
+AVBIN_LOCAL = 'avbin'
+
+def fetch_dep_list():
+    print('Fetching dependencies list from ' + DEP_LIST_URL + '-' + platform.system() + '...')
+    download(DEP_LIST_URL + '-' + platform.system(), DEP_LIST_LOCAL)
+
+def install_dep():
+    print('Downloading dependencies...')
+    os.mkdir('downloads')
+    fetch_dep_list()
+    top_dir = os.getcwd()
+    os.chdir('downloads')
+    dep_list = open(DEP_LIST_LOCAL)
+    pyg_version = dep_list.readline().rstrip('\n')
+    lib_url = dep_list.readline().rstrip('\n')
+    # first pyglet
+    downloads_dir = os.getcwd()
+    print('Downloading pyglet from ' + lib_url + '...')
+    download(lib_url, PYGLET_LOCAL)
+    print('Extracting pyglet...')
+    zipfile.ZipFile(PYGLET_LOCAL, 'r').extractall(downloads_dir)
+    print('Installing pyglet...')
+    os.chdir(pyg_version)
+    if os.name == 'posix':
+        os.system('sudo python setup.py install')
+    else:
+        os.system('python setup.py install')
+    os.chdir(downloads_dir)
+    lib_url = dep_list.readline().rstrip('\n')
+    print('Downloading AVBin from ' + lib_url + '...')
+    download(lib_url, AVBIN_LOCAL)
+    print('Install AVBin...')
+    if os.name == 'posix':
+        os.system('sudo sh ./' + AVBIN_LOCAL)
+    else:
+        os.system(AVBIN_LOCAL)
+
+install_dep()
 
 ext_modules = [Extension(name, [f]) for name, f in get_modules()]
 
