ba0198064130cdd8cfbb0570784e7c8e39757b98
Ronald Brad Murphy <solo.dev@gmail.com>
2013-05-01 17:55:59 -0400

    remove run.bat, runs.bat. depreciated.

--- run.bat
+++
@@ -1,2 +0,0 @@
-c:\Python\python.exe %1 --disable-auto-save --disable-save %2 %3 %4 %5 %6 %7 %8 %9
-cleanup.bat
--- runw.bat
+++
@@ -1,2 +0,0 @@
-c:\Python\python.exe %1 --disable-auto-save --disable-save --seed 280007932345949988591697400339756987054 %2 %3 %4 %5 %6 %7 %8 %9
-cleanup.bat
