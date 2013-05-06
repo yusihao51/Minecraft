yusihao51/Minecraft

commit ce7adfb9c33a57f31b34cbe243069f65ab1cbd2a

Fixed saving (but seeds are still not saved correctly).

savingsystem.py
@@ -76,6 +76,8 @@ def save_world(window, game_dir, world=None):
                         for z in xrange(cz, cz+8):
                             blk = blocks.get((x,y,z), air).id
                             if blk:
+                                if isinstance(blk, int):
+                                    blk = BlockID(blk)
                                 fstr += structuchar2.pack(blk.main, blk.sub)
                             else:
                                 fstr += null2
@@ -141,9 +143,10 @@ def load_region(model, world=None, region=None, sector=None):
                                 for z in xrange(cz, cz+8):
                                     read = fstr[fpos:fpos+2]
                                     fpos += 2
-                                    if read != null2:
+                                    unpacked = structuchar2.unpack(read)
+                                    if read != null2 and unpacked in BLOCKS_DIR:
                                         position = x,y,z
-                                        blocks[position] = BLOCKS_DIR[structuchar2.unpack(read)]
+                                        blocks[position] = BLOCKS_DIR[unpacked]
                                         sectors[(x/SECTOR_SIZE, y/SECTOR_SIZE, z/SECTOR_SIZE)].append(position)
 
 @performance_info

controllers.py
@@ -345,7 +345,7 @@ class GameController(Controller):
                                 if localx != 0 or localz != 0 or (localy != 0 and localy != -1):
                                     self.model.add_block(previous, current_block)
                                     self.item_list.remove_current_block()
-                elif self.item_list.get_current_block() and self.item_list.get_current_block().regenerated_health != 0 and self.player.health < self.player.max_health:
+                elif self.item_list.get_current_block() and hasattr(self.item_list.get_current_block(), 'regenerated_health') and self.item_list.get_current_block().regenerated_health != 0 and self.player.health < self.player.max_health:
                     self.player.change_health(self.item_list.get_current_block().regenerated_health)
                     self.item_list.get_current_block_item().change_amount(-1)
                     self.item_list.update_health()

commit 2c28f263ef2848432e8e73284d3bb9d4c6242a0a

Optimized chunk saving and loading speed (20% faster) Fixed slowdowns caused by ce7adfb9c33a57f31b34cbe243069f65ab1cbd2a

savingsystem.py
@@ -55,9 +55,9 @@ def save_sector_to_string(blocks, secpos):
         for y in xrange(cy, cy+8):
             for z in xrange(cz, cz+8):
                 blk = blocks.get((x,y,z), air).id
-                if blk:
-                    if isinstance(blk, int):
-                        blk = BlockID(blk)
+                if blk is not air:
+                    #if isinstance(blk, int): # When does this occur? Its expensive and I don't see it triggering
+                    #    blk = BlockID(blk)
                     fstr += structuchar2.pack(blk.main, blk.sub)
                 else:
                     fstr += null2
@@ -131,10 +131,11 @@ def load_region(world, world_name=None, region=None, sector=None):
                                 for z in xrange(cz, cz+8):
                                     read = fstr[fpos:fpos+2]
                                     fpos += 2
-                                    unpacked = structuchar2.unpack(read)
-                                    if read != null2 and unpacked in BLOCKS_DIR:
+                                    if read != null2:
                                         position = x,y,z
-                                        blocks[position] = BLOCKS_DIR[unpacked]
+                                        try: blocks[position] = BLOCKS_DIR[structuchar2.unpack(read)]
+                                        except KeyError as e:
+                                            print "load_region: Invalid Block", e
                                         sectors[(x/SECTOR_SIZE, y/SECTOR_SIZE, z/SECTOR_SIZE)].append(position)
 
 def load_player(player, world):